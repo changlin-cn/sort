b313185525255a0bca63f888335f587eae22f3fb
Hechanglin <542177932@qq.com>
2020-12-29 11:43:51 +0800

    希尔排序

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { bubbleSort } from './bubble-sort';
 export { insertionSort } from './insertion-sort';
 export { selectionSort } from './selection-sort';
 export { quickSort } from './quick-sort';
+export { shellSort } from './shell-sort';
---
+++ src/shell-sort.ts
@@ -0,0 +1,38 @@
+import { compareFnDefalut } from './compare-fn-defalut';
+
+function createGaps(length: number): number[] {
+  const gaps = [1];
+
+  while (gaps[gaps.length - 1] < length / 3) {
+    gaps.push(gaps[gaps.length - 1] * 3 + 1);
+  }
+  return gaps;
+}
+
+export function shellSort<T>(arr: T[], compareFn: (a: T, b: T) => number = compareFnDefalut): T[] {
+  const length = arr.length;
+  const result = arr.slice();
+  if (length < 2) {
+    return result;
+  }
+  const gaps = createGaps(length);
+
+  while (gaps.length > 0) {
+    const gap = <number>gaps.pop();
+    for (let i = gap; i < length; i += gap) {
+      for (let j = i; j > 0; j -= gap) {
+        const a = result[j];
+        const b = result[j - gap];
+        const compareReault = compareFn(a, b);
+        if (compareReault < 0) {
+          result[j] = b;
+          result[j - gap] = a;
+          continue;
+        }
+        break;
+      }
+    }
+  }
+
+  return result;
+}
