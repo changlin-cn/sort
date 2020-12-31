d40df434aebd281fbf4e73aa6573053f5d9d7d52
Hechanglin <542177932@qq.com>
2020-12-31 21:03:09 +0800

    添加时间比较

---
+++ operation-time/index.ts
@@ -0,0 +1,40 @@
+import {
+  bubbleSort,
+  insertionSort,
+  selectionSort,
+  quickSort,
+  shellSort,
+  mergeSortFromBottom,
+  mergeSortFromTop,
+  heapSort,
+} from '../src/index';
+
+[20, 200, 2000, 20000, 100000, 100000, 100000].forEach((len) => {
+  const dataRandom: number[] = [];
+  while (dataRandom.length < len) {
+    dataRandom.push(parseInt(`${(Math.random() * 10000).toFixed(0)}`));
+  }
+
+  console.log(`${len} start --------------`);
+  const dataCopy = dataRandom.slice();
+  console.time('array.sort:');
+  dataCopy.sort((a, b) => a - b);
+  console.timeEnd('array.sort:');
+
+  [
+    bubbleSort,
+    insertionSort,
+    selectionSort,
+    quickSort,
+    shellSort,
+    mergeSortFromBottom,
+    mergeSortFromTop,
+    heapSort,
+  ].forEach((fn) => {
+    console.time(fn.name);
+    fn(dataRandom);
+    console.timeEnd(fn.name);
+  });
+
+  console.log(`${len} end -------------`);
+});
